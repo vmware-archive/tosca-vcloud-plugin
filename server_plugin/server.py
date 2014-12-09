a08f67193d4bc26e59341e8e6d76e1912d3e1d3d
Oleksandr Chyrko <aleksandr.chirko@gmail.com>
2014-12-09 18:10:19 +0200

    Add server plugin
    Add server type definition.
    Add implementation for vapp create and delete.
    Add common code.

---
+++ server_plugin/server.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+from cloudify import ctx
+from cloudify.decorators import operation
+from cloudify import exceptions as cfy_exc
+
+from vcloud_plugin_common import (transform_resource_name,
+                                  wait_for_task,
+                                  with_vcd_client)
+
+VCLOUD_VAPP_NAME = 'vcloud_vapp_name'
+
+
+@operation
+@with_vcd_client
+def create(vcd_client, **kwargs):
+    server = {
+        'name': ctx.instance.id,
+    }
+    server.update(ctx.node.properties['server'])
+    transform_resource_name(server, ctx)
+    required_params = ('catalog', 'template')
+    missed_params = set(required_params) - set(server.keys())
+    if len(missed_params) > 0:
+        raise cfy_exc.NonRecoverableError(
+            "{0} server properties must be specified"
+            .format(list(missed_params)))
+    create_args = {
+        '--deploy': True,
+        '--on': True,
+        '--blocking': False
+        }
+    success, result = vcd_client.create_vApp(server['name'],
+                                             server['template'],
+                                             server['catalog'],
+                                             create_args)
+
+    if success is False:
+        raise cfy_exc.NonRecoverableError(
+            "Could not create vApp: {0}".format(result))
+
+    task = result.get_Tasks().get_Task()[0]
+    wait_for_task(vcd_client, task)
+    ctx.instance.runtime_properties[VCLOUD_VAPP_NAME] = server['name']
+
+
+@operation
+@with_vcd_client
+def start(vcd_client, **kwargs):
+    pass
+
+
+@operation
+@with_vcd_client
+def stop(vcd_client, **kwargs):
+    pass
+
+
+@operation
+@with_vcd_client
+def delete(vcd_client, **kwargs):
+    vapp_name = ctx.instance.runtime_properties[VCLOUD_VAPP_NAME]
+    delete_args = {
+        '--blocking': True,
+        '--json': True
+        }
+    vapp = vcd_client.get_vApp(vapp_name)
+    vapp.delete(delete_args)
+    del ctx.instance.runtime_properties[VCLOUD_VAPP_NAME]
+
+
+@operation
+@with_vcd_client
+def get_state(vcd_client, **kwargs):
+    pass
---
+++ vcloud_plugin_common/__init__.py
@@ -0,0 +1,142 @@
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+import atexit
+from functools import wraps
+import json
+import os
+import requests
+import time
+
+from pyvcloud import vcloudair
+from pyvcloud.schema.vcd.v1_5.schemas.vcloud import taskType
+
+from cloudify import ctx
+from cloudify import exceptions as cfy_exc
+
+TASK_RECHECK_TIMEOUT = 2
+TASK_STATUS_SUCCESS = 'success'
+TASK_STATUS_ERROR = 'error'
+
+
+def transform_resource_name(res, ctx):
+
+    if isinstance(res, basestring):
+        res = {'name': res}
+
+    if not isinstance(res, dict):
+        raise ValueError("transform_resource_name() expects either string or "
+                         "dict as the first parameter")
+
+    pfx = ctx.bootstrap_context.resources_prefix
+
+    if not pfx:
+        return res['name']
+
+    name = res['name']
+    res['name'] = pfx + name
+
+    if name.startswith(pfx):
+        ctx.logger.warn("Prefixing resource '{0}' with '{1}' but it "
+                        "already has this prefix".format(name, pfx))
+    else:
+        ctx.logger.info("Transformed resource name '{0}' to '{1}'".format(
+                        name, res['name']))
+
+    return res['name']
+
+
+class Config(object):
+
+    VCLOUD_CONFIG_PATH_ENV_VAR = 'VCLOUD_CONFIG_PATH'
+    VCLOUD_CONFIG_PATH_DEFAULT = '~/vcloud_config.json'
+
+    def get(self):
+        cfg = {}
+        env_name = self.VCLOUD_CONFIG_PATH_ENV_VAR
+        default_location_tpl = self.VCLOUD_CONFIG_PATH_DEFAULT
+        default_location = os.path.expanduser(default_location_tpl)
+        config_path = os.getenv(env_name, default_location)
+        try:
+            with open(config_path) as f:
+                cfg = json.loads(f.read())
+        except IOError:
+            pass
+        return cfg
+
+
+class VcloudDirectorClient(object):
+
+    config = Config
+
+    def get(self, config=None, *args, **kw):
+        static_config = self.__class__.config().get()
+        cfg = {}
+        cfg.update(static_config)
+        if config:
+            cfg.update(config)
+        return self.connect(cfg)
+
+    def connect(self, cfg):
+        url = cfg.get('url')
+        username = cfg.get('username')
+        password = cfg.get('password')
+        token = cfg.get('token')
+        service = cfg.get('service')
+        vdc = cfg.get('vdc')
+        if not (all([url, token]) or all([url, username, password])):
+            raise cfy_exc.NonRecoverableError(
+                "Login credentials must be specified")
+        if not (service and vdc):
+            raise cfy_exc.NonRecoverableError(
+                "vCloud service and vDC must be specified")
+
+        vca = vcloudair.VCA()
+        success = vca.login(url, username, password, token)
+        if success is False:
+            raise cfy_exc.NonRecoverableError("Invalid login credentials")
+        else:
+            atexit.register(vca.logout())
+
+        vcloud_director = vca.get_vCloudDirector(service, vdc)
+        if vcloud_director is None:
+            raise cfy_exc.NonRecoverableError(
+                "Could not get vCloud Director reference")
+        else:
+            return vcloud_director
+
+
+def with_vcd_client(f):
+    @wraps(f)
+    def wrapper(*args, **kw):
+        config = ctx.node.properties.get('vcloud_config')
+        client = VcloudDirectorClient().get(config=config)
+        kw['vcd_client'] = client
+        return f(*args, **kw)
+    return wrapper
+
+
+def wait_for_task(vcd_client, task):
+    status = task.get_status()
+    while status != TASK_STATUS_SUCCESS:
+        if status == TASK_STATUS_ERROR:
+            error = task.get_Error()
+            raise cfy_exc.NonRecoverableError(
+                "Error during task execution: {0}".format(error))
+        else:
+            time.sleep(TASK_RECHECK_TIMEOUT)
+            response = requests.get(task.get_href(),
+                                    headers=vcd_client.headers)
+            task = taskType.parseString(response.content, True)
+            status = task.get_status()
