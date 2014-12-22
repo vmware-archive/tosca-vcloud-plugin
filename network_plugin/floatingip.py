d039387b93af93dfe03517a2462b9c57e7bbb291
kostya13 <Konstantin_Ilyashenko@epam.com>
2014-12-22 16:12:50 +0400

    remove file headers and empty tests. remove empty implementations

--- network_plugin/floatingip.py
+++ network_plugin/floatingip.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 from cloudify import ctx
 from cloudify import exceptions as cfy_exc
 from cloudify.decorators import operation
@@ -21,19 +7,6 @@ from vcloud_plugin_common import with_vcd_client, wait_for_task
 CREATE = 1
 DELETE = 2
 VCLOUD_VAPP_NAME = 'vcloud_vapp_name'
-
-
-@operation
-@with_vcd_client
-def create(vcloud_client, **kwargs):
-    # may be usefull, if use dynamic ip selection
-    pass
-
-
-@operation
-@with_vcd_client
-def delete(vcd_client, **kwargs):
-    pass
 
 
 @operation
