ab6695f613ece42c6637daf18001820286cbf280
kostya13 <konstantin@gigaspaces.com>
2014-12-30 09:13:34 +0400

    add existiong rule check

--- network_plugin/__init__.py
+++ network_plugin/__init__.py
@@ -9,3 +9,15 @@ def check_ip(address):
         raise cfy_exc.NonRecoverableError(
             "Incorrect Ip addres: {0}".format(address))
     return address
+
+def collectExternalIps(gateway):
+    ips = []
+    if gateway:
+        for natRule in gateway.get_nat_rules():
+            rule = natRule.get_GatewayNatRule()
+            rule_type = natRule.get_RuleType()
+            if rule_type == "DNAT":
+                ips.append(rule.get_OriginalIp())
+            else:
+                ips.append(rule.get_TranslatedIp())
+                return ips
