f4cf4fbeb08e89628769211e5bb261dd2c37ea85
Kostya <konstantin@gigaspaces.com>
2015-03-13 13:43:35 +0400

    Add validation for port and keypair node types

--- network_plugin/keypair.py
+++ network_plugin/keypair.py
@@ -1,11 +1,13 @@
 from cloudify import ctx
 from cloudify import exceptions as cfy_exc
 from cloudify.decorators import operation
-from vcloud_plugin_common import with_vca_client, get_vcloud_config
-from network_plugin import check_ip, get_vm_ip, save_gateway_configuration
+from vcloud_plugin_common import with_vca_client, get_mandatory
+import os.path
 
 
 @operation
 @with_vca_client
 def creation_validation(vca_client, **kwargs):
-    pass
+    key = get_mandatory(ctx.node.properties, 'private_key_path')
+    if not os.path.isfile(key):
+        raise cfy_exc.NonRecoverableError("Private key file {0} is absent".format(key))
