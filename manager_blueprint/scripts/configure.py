6e9525787c9f26af881942cc869273a6154f1970
Denis Pauk <pauk.denis@gmail.com>
2015-03-19 17:15:37 +0200

    CFY-2152 vCloud plugin - bootstap context: add development packages install (Ubuntu/deb version)

--- manager_blueprint/scripts/configure.py
+++ manager_blueprint/scripts/configure.py
@@ -8,6 +8,7 @@ import vcloud_plugin_common
 
 def configure(vcloud_config):
     _copy_vsphere_configuration_to_manager(vcloud_config)
+    _update_vm()
 
 
 def _copy_vsphere_configuration_to_manager(vcloud_config):
@@ -16,3 +17,21 @@ def _copy_vsphere_configuration_to_manager(vcloud_config):
         json.dump(vcloud_config, f)
     fabric.api.put(tmp,
                    vcloud_plugin_common.Config.VCLOUD_CONFIG_PATH_DEFAULT)
+
+
+def _get_distro():
+    """ detect current distro """
+    return fabric.api.run('python -c "import platform; print platform.dist()[0]"')
+
+
+def _update_vm():
+    """ install some packeges for future deployments creation """
+    distro = _get_distro()
+    if 'Ubuntu' in distro:
+        # update system to last version
+        fabric.api.run("sudo apt-get update -q -y 2>&1")
+        fabric.api.run("sudo apt-get dist-upgrade -q -y 2>&1")
+        # install:
+        # * zram-config for minimize out-of-memory cases with zswap
+        # * other packages for create deployments from source
+        fabric.api.run("sudo apt-get install zram-config gcc python-dev libxml2-dev libxslt-dev -q -y 2>&1")
