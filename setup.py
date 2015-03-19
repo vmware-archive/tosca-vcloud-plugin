7c1080be7967042344792d7356e1bc80d66ff003
Oleksandr Chyrko <oleksandr@gigaspaces.com>
2015-03-19 14:54:05 +0200

    Use pyvcloud 12c2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     description='Cloudify plugin for vmWare vCloud infrastructure.',
     install_requires=[
         'cloudify-plugins-common==3.2a6',
-        'pyvcloud==10',
+        'pyvcloud==12c2',
         'requests==2.4.3',
         'IPy==0.81'
     ]
