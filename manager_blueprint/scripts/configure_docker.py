f28d3fc2278fc51003de6a8920a961f662e68bb4
Denis Pauk <pauk.denis@gmail.com>
2015-04-07 12:54:42 +0300

    CFY-2483: drop check for host linux version

--- manager_blueprint/scripts/configure_docker.py
+++ manager_blueprint/scripts/configure_docker.py
@@ -2,25 +2,17 @@ import fabric
 
 
 def configure(vcloud_config):
-    _update_vm()
+    _update_container()
 
 
-def _get_distro():
-    """ detect current distro """
-    return fabric.api.run(
-        'python -c "import platform; print platform.dist()[0]"')
-
-
-def _update_vm():
+def _update_container():
     """ install some packeges for future deployments creation """
-    distro = _get_distro()
-    if 'Ubuntu' in distro:
-        # update system to last version
-        fabric.api.run("sudo docker exec -i -t cfy apt-get "
-                       "update -q -y 2>&1")
-        fabric.api.run("sudo docker exec -i -t cfy apt-get "
-                       "dist-upgrade -q -y 2>&1")
-        # install:
-        fabric.api.run("sudo docker exec -i -t cfy apt-get "
-                       "install gcc python-dev libxml2-dev libxslt-dev "
-                       "zlib1g-dev -q -y 2>&1")
+    # update system to last version
+    fabric.api.run("sudo docker exec -i -t cfy apt-get "
+                   "update -q -y 2>&1")
+    fabric.api.run("sudo docker exec -i -t cfy apt-get "
+                   "dist-upgrade -q -y 2>&1")
+    # install:
+    fabric.api.run("sudo docker exec -i -t cfy apt-get "
+                   "install gcc python-dev libxml2-dev libxslt-dev "
+                   "zlib1g-dev -q -y 2>&1")
