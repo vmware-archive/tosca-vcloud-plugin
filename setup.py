737fd9a5a3a18cbba3826f9163980ef8335eedb5
Kostya <konstantin@gigaspaces.com>
2015-05-28 11:55:30 +0400

    CFY-2363 pyvcloud - can't set vm's cpu and memory size for templates from Public Catalog

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     description='Cloudify plugin for vmWare vCloud infrastructure.',
     install_requires=[
         'cloudify-plugins-common>=3.2',
-        'pyvcloud==12',
+        'pyvcloud==13rc7',
         'requests==2.4.3',
         'IPy==0.81',
         'PyYAML==3.10'
