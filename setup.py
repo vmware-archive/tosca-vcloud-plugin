5de1860d8f570546e63fd8b07b2ecb6773b0c1a8
Denis Pauk <pauk.denis@gmail.com>
2018-03-27 15:06:02 +0300

    Bump version for include all last fixes

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-vcloud-plugin',
-    version='1.4',
+    version='1.4.1',
     packages=[
         'vcloud_plugin_common',
         'vcloud_server_plugin',
